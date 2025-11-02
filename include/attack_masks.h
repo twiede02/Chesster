faacff23b287741ee64a0eb0a435d727b4658317
Tobias Wiedemann <towi00001@stud.uni-saarland.de>
2025-11-02 23:04:02 +0100

    Attack masks for magic bitboards

--- include/attack_masks.h
+++ include/attack_masks.h
@@ -5,3 +5,6 @@
 
 extern std::vector<uint64_t> knight_masks;
 extern std::vector<uint64_t> king_masks;
+extern std::vector<uint64_t> rook_blocker_masks;
+extern std::vector<uint64_t> bishop_blocker_masks;
+
